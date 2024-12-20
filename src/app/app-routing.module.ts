1499c70fc5b014ac06e2345e4515509e041bc055
Goutam2323 <bishtgoutam3@gmail.com>
2024-12-21 00:57:37 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductFormComponent } from './forms/product-form/product-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'product-form', component: ProductFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductFormComponent } from './forms/product-form/product-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
